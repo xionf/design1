39459bc625e8002d00f400e6ec374e208a36f568
xiongf <1770363010@qq.com>
2021-06-19 23:11:54 +0800

    责任链模式

---
+++ src/chainOfResponsibility/Handler.java
@@ -0,0 +1,13 @@
+package chainOfResponsibility;
+
+abstract class Handler {
+    private Handler next;
+    public void setNext(Handler next) {
+        this.next = next;
+    }
+    public Handler getNext() {
+        return next;
+    }
+    //处理请求的方法
+    public abstract void handleRequest(String request);
+}
