b6c8c6714f6185f03577b851d6a68b033470ea95
xiongf <1770363010@qq.com>
2021-06-19 22:43:31 +0800

    外观模式

---
+++ src/facade/FacadePattern.java
@@ -0,0 +1,8 @@
+package facade;
+
+public class FacadePattern {
+    public static void main(String[] args) {
+        Facade f = new Facade();
+        f.method();
+    }
+}
---
+++ src/facade/SubSystem03.java
@@ -0,0 +1,7 @@
+package facade;
+
+class SubSystem03 {
+    public void method3() {
+        System.out.println("子系统03的method3()被调用！");
+    }
+}
