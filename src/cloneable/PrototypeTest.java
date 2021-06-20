6eba4ee3e3395c3790e83e3781e0f2b8ecec8088
xiongf <1770363010@qq.com>
2021-06-20 15:16:12 +0800

    原型模式

---
+++ src/cloneable/PrototypeTest.java
@@ -0,0 +1,9 @@
+package cloneable;
+
+public class PrototypeTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Realizetype obj1 = new Realizetype();
+        Realizetype obj2 = (Realizetype) obj1.clone();
+        System.out.println("obj1==obj2?" + (obj1 == obj2));
+    }
+}
