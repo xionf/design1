a8cf3ed169c97579fba22ac7452a99bd8e509c7c
xiongf <1770363010@qq.com>
2021-06-20 15:03:27 +0800

    访问者模式

--- src/victor/ObjectStructure.java
+++ src/victor/ObjectStructure.java
@@ -3,15 +3,19 @@ package victor;
 import java.util.*;
 class ObjectStructure {
     private List<Element> list = new ArrayList<Element>();
+
     public void accept(Visitor visitor) {
         Iterator<Element> i = list.iterator();
         while (i.hasNext()) {
             ((Element) i.next()).accept(visitor);
         }
     }
+
     public void add(Element element) {
         list.add(element);
     }
+
     public void remove(Element element) {
         list.remove(element);
     }
+}
